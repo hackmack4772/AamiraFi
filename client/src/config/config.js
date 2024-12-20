13d09f5b6a8823c6a0aff7adb2a893911e5d2b9e
hackmack4772 <aamir.saleem@shinedezign.com>
2024-12-20 18:17:21 +0530

    link chnage

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://54.194.136.87:5000/";
+const URL = "https://video-call-server-gm7i.onrender.com";
 // const URL = "https://video-call-server-gm7i.onrender.com";
 
 export const socket = io(URL);
